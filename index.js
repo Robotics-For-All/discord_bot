4385ddbf24f5e950470fab96568634951bf95c08
Eesha-Jain <eesha.jain801@gmail.com>
2021-12-31 17:25:53 -0800

    created discord bot

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const Discord = require("discord.js")
-const client = new Discord.Client()
+require('dotenv').config();
+const Discord = require('discord.js');
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 client.on("message", msg => {
   if (msg.content === "welcome") {
